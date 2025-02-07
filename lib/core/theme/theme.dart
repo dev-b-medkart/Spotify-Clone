ddf219331cf7dd20b387b5203db2ffb3b42d4d8e
dev-b-medkart <dev.b@medkart.in>
2025-02-07 16:10:23 +0530

    first commit

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/theme/app_pallete.dart';
+
+class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(color: color, width: 3));
+  static final darkThemeMode = ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: Pallete.backgroundColor,
+      inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.all(27),
+          enabledBorder: _border(Pallete.borderColor),
+          focusedBorder: _border(Pallete.gradient2))
+  );
+}
